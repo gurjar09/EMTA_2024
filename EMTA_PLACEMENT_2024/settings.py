3b483389b51cee8ffab12cffb41af9ad9f75362e
sagar gurjar <sagarchhalotre@gmail.com>
2024-05-06 14:05:19 +0530

    first commit

--- EMTA_PLACEMENT_2024/settings.py
+++ EMTA_PLACEMENT_2024/settings.py
@@ -12,15 +12,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = ['nekhbaesdfhasbxcasd4d5w24e3w4adsqwe41']
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG","False").lower() == "true"
-DEBUG = 'True'
+DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-# ALLOWED_HOSTS = ['emta-2024-9.onrender.com']
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['emta-2024-9.onrender.com']
+# ALLOWED_HOSTS = []
 
 
 
